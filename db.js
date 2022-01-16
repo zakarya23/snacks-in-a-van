620ea6298dd8caf8e36f020e0bc207c847b75b42
Zakarya Ahmed Butt <zakarya.butt@student.unimelb.edu.au>
2022-01-16 22:29:17 +0500

    env file hidden

--- db.js
+++ db.js
@@ -1,26 +1,28 @@
 // setup Mongoose
 const mongoose = require('mongoose')
 var Schema = mongoose.Schema
-const dotenv = require('dotenv');
-dotenv.config();
-let mongoPassword = process.env.MONGO_PASSWORD;
-console.log(mongoPassword);
-let connectionURL =  "mongodb+srv://newuser1:"+mongoPassword+"@cluster.sacsx.mongodb.net/foodbuddydb?retryWrites=true&w=majority"
+const dotenv = require('dotenv')
+dotenv.config()
+let connectionURL = process.env.DB_URI
 
 try {
-    // Connect to the MongoDB cluster
-     mongoose.connect(
-      connectionURL,
-      {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, dbName: 'foodbuddydb'},
-      () => console.log("Mongoose is connected")
-    );
-
-  } catch (e) {
-    console.log("could not connect");
-  }
+	// Connect to the MongoDB cluster
+	mongoose.connect(
+		connectionURL,
+		{
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+			useCreateIndex: true,
+			dbName: 'foodbuddydb',
+		},
+		() => console.log('Mongoose is connected')
+	)
+} catch (e) {
+	console.log('could not connect')
+}
 const db = mongoose.connection
 // event handlers
 db.on('error', console.error.bind(console, 'connection error:'))
 db.once('open', () => {
-    console.log('connected to Mongo')
+	console.log('connected to Mongo')
 })
